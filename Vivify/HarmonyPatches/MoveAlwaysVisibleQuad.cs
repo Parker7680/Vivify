2b78140c2f2d9008254c3da32d1cac7aaae9e896
Aeroluna <40481393+Aeroluna@users.noreply.github.com>
2026-05-06 09:26:01 -0700

    i guess it doesnt have to be that far away

--- Vivify/HarmonyPatches/MoveAlwaysVisibleQuad.cs
+++ Vivify/HarmonyPatches/MoveAlwaysVisibleQuad.cs
@@ -7,10 +7,11 @@ namespace Vivify.HarmonyPatches;
 [HeckPatch(PatchType.Features)]
 internal static class MoveAlwaysVisibleQuad
 {
+    // still dont really know what this thing is for, but it has a shadowcaster so lets bump it out of view.
     [HarmonyPostfix]
     [HarmonyPatch(typeof(AlwaysVisibleQuad), nameof(AlwaysVisibleQuad.OnEnable))]
     private static void MoveIt(AlwaysVisibleQuad __instance)
     {
-        __instance.transform.position = new Vector3(0, -1000, 0);
+        __instance.transform.position = new Vector3(0, -10, 0);
     }
 }
